b5648c179c18aab2ef6a27674f96f163ad646755
shruti121824 <shruti.jamgade@capgemini.com>
2017-06-05 16:55:16 +0530

    Add files via upload

---
+++ DevOpsDay1/src/javaDemo/Demo.java
@@ -0,0 +1,16 @@
+package javaDemo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=a+b;
+		
+		System.out.println("Addition is:"+c);
+
+
+	}
+
+}
